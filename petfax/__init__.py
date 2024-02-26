010ddb7c2b1059f1c04c2971dc1289192887b40a
NeNeZoRr <zorrillalin@gmail.com>
2024-02-26 12:28:20 -0500

    update

---
+++ petfax/__init__.py
@@ -0,0 +1,26 @@
+# config                    
+from flask import Flask
+from flask_migrate import Migrate
+
+# factory
+def create_app():
+    app = Flask(__name__)
+    # app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://{username}:{password}@localhost:5432/PetFax'
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+    from . import models 
+    models.db.init_app(app)
+    migrate = Migrate(app, models.db)
+
+
+    @app.route('/')
+    def hello(): 
+        return 'Hello, PetFax!'
+
+    from . import pet
+    app.register_blueprint(pet.bp)
+
+    from . import fact
+    app.register_blueprint(fact.bp)
+
+    return app
